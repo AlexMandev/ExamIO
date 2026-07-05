c21b05da2a3c58990ac78beb7dfbefb444338901
T3Nzw <t3nszw@gmail.com>
2026-07-05 04:19:39 +0300

    fix migration

--- src/main/resources/db-migrations/V7__add_deadline_to_submissions.sql
+++ src/main/resources/db-migrations/V7__add_deadline_to_submissions.sql
@@ -1,2 +1,10 @@
 ALTER TABLE submissions
-ADD deadline TIMESTAMPTZ NOT NULL;
+ADD deadline TIMESTAMPTZ;
+
+UPDATE submissions s
+SET deadline = s.started_at + make_interval(mins => e.time_limit_minutes)
+FROM exams e
+WHERE s.exam_id = e.id AND s.deadline IS NULL;
+
+ALTER TABLE submissions
+ALTER COLUMN deadline SET NOT NULL;
